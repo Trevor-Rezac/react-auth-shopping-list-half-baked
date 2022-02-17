4244a1383bee1767b066f3976d034305a90b132c
TrevorRezac <trev.rezac@gmail.com>
2022-02-16 16:21:17 -0800

    fixed redirects, last commit sent too early

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ export default function App() {
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
-              {currentUser ? <ListPage /> : <AuthPage setCurrentUser={setCurrentUser} />}
+              {currentUser ? <Redirect to='/shopping-list' /> : <AuthPage setCurrentUser={setCurrentUser} />}
             </Route>
             <Route exact path="/shopping-list">
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
